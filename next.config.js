6e86e4fd9efcfd117252bbcae5a3638989898f09
joehenderson99-eng <joehenderson99@gmail.com>
2026-03-02 00:23:59 -0800

    Change export method and add comment in next.config.js
    Changed export method for nextConfig and added a comment about API routes.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  // DO NOT use output: "export" if you need /api routes
 };
-export default nextConfig;
+
+module.exports = nextConfig;
